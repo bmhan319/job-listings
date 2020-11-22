7fc6c46eb07691465d798a57c765b616067f16f6
Brian Han <brianhan@brians-mbp.lan>
2020-11-21 17:53:05 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+import './css/header.css'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
       </div>
     )
   }
