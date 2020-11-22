066ca438a863710d0915e8d4206d4f7263c5b26e
Brian Han <brianhan@brians-mbp.lan>
2020-11-22 11:44:16 -0800

    created api call on load to get json data from local file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import './css/main.css'
 import './css/listings.css'
 
 export default class App extends Component {
+  state = {
+    gender: 'male',
+    number: 1
+  }
+
+  callApi = async () => {
+    const header = {'Content-Type': 'application/json',
+                    'Accept': 'application/json'}
+    const response = await fetch( 'data.json', {headers: header} )
+    const data = await response.json()
+    console.log(data)
+  }
+
+  componentDidMount () {
+    window.onload = () => {
+      this.callApi()
+    }
+  }
+
   render() {
     return (
       <div className="App">
