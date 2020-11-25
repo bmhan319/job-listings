d00bbad218a134760293655a92cd82de145941d6
Brian Han <brianhan@brians-mbp.lan>
2020-11-24 22:06:36 -0800

    working on filtering out each listing based on tag search

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -15,11 +15,14 @@ export default class Listings extends Component {
   componentDidMount () {
     this.displayNew(this.props.item.new)
     this.displayFeature(this.props.item.featured)
+    document.querySelectorAll('.languageTag').forEach( item => (
+      console.log(item.dataset.tag )
+    ))
   }
 
   render() {
     return (
-      <li id={`listingComponent${this.props.item.id}`} className="listingComponent">
+      <li id={`listingComponent${this.props.item.id}`} className="listingComponent" data-role={this.props.item.role} data-level={this.props.item.level} data-languages={this.props.item.languages} data-tools={this.props.item.tools} >
         <div className="logoContainer">
           <img className="logoImg" src={this.props.item.logo} alt={this.props.item.company}/>
         </div>
@@ -38,13 +41,13 @@ export default class Listings extends Component {
         </p>
         <hr className="listingDivider" />
         <ul className="tagsContainer">
-          <li key={this.props.item.company + this.props.item.role}  onClick={()=>{this.props.addTags(this.props.item.role)}} className="roleTag tag">{this.props.item.role}</li>
-          <li key={this.props.item.company + this.props.item.level} onClick={()=>{this.props.addTags(this.props.item.level)}} className="levelTag tag">{this.props.item.level}</li>
+          <li key={this.props.item.company + this.props.item.role}  onClick={()=>{this.props.addTags(this.props.item.role)}} data-tag={this.props.item.role} className={`roleTag tag ${this.props.item.role}`}>{this.props.item.role}</li>
+          <li key={this.props.item.company + this.props.item.level} onClick={()=>{this.props.addTags(this.props.item.level)}} data-tag={this.props.item.level} className={`levelTag tag ${this.props.item.level}`}>{this.props.item.level}</li>
           { this.props.item.languages.map( item => (
-            <li key={this.props.item.company + item} onClick={()=>{this.props.addTags(item)}} className="languageTag tag">{item}</li>
+            <li key={this.props.item.company + item} onClick={()=>{this.props.addTags(item)}} data-tag={item} className={`languageTag tag ${item}`}>{item}</li>
           ))}
           { this.props.item.tools.map( item => (
-            <li key={this.props.item.company + item} onClick={()=>{this.props.addTags(item)}} className="toolTag tag">{item}</li>
+            <li key={this.props.item.company + item} onClick={()=>{this.props.addTags(item)}} data-tag={item} className={`toolTag tag ${item}`}>{item}</li>
           ))}
           
         </ul>
