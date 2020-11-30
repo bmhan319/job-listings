330188b3ca6e161725740c6fe57daeccf5678a77
Brian Han <brianhan@brians-mbp.lan>
2020-11-30 01:11:48 -0800

    basic code cleanup...will need to do more

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './css/header.css'
 import './css/main.css'
 import './css/filter.css'
 import './css/listings.css'
-var ownTags = []
+
+let ownTags = []
+
 export default class App extends Component {
   state = {
     'new': 'NEW!',
@@ -29,43 +31,15 @@ export default class App extends Component {
 
   
   addTags = (tag) => {
-    let jsonArray =[]
     this.openFilter()
 
     if (this.state.tags.indexOf(tag) === -1) {
       ownTags.push(tag)
       this.setState({
-        tags:[...this.state.tags, tag]
+        tags: ownTags
       })
     }
-    
-
-    this.state.json.forEach(listing => {
-      let array = []
-      array.push(listing.role)
-      array.push(listing.level)
-      array.push( listing.languages.forEach(item=> {
-        array.push(item)
-      })) 
-      array.push( listing.tools.forEach(item=> {
-        array.push(item)
-      })) 
-
-      let count = 0
-      for (var i = 0; i < ownTags.length; i++) {
-        for (var k = 0; k < array.length; k++) {
-          if (ownTags[i] === array[k]) {
-            count++
-          }
-        }
-      }
-      if (count === ownTags.length) {
-        jsonArray.push(listing)
-      }
-    })
-    this.setState({
-      'listings': jsonArray
-    })
+    this.filterTag()
   }
 
   removeTags = (tag) => {
@@ -73,17 +47,23 @@ export default class App extends Component {
     let index = this.state.tags.indexOf(tag)
     array.splice(index, 1)
     this.setState({tags: array})
-    if (this.state.tags.length === 1) {
-      this.clearFilter()
-    }
 
     ownTags.forEach( (tagItem,ind) => {
       if (tag === tagItem) { 
         ownTags.splice(ind, 1)
       }
     })
-    console.log(ownTags)
+    if (ownTags.length === 0) {
+      this.clearFilter()
+    }
+    this.setState({
+      tags: ownTags
+    })
 
+    this.filterTag()
+  }
+
+  filterTag = () => {
     let jsonArray =[]
     this.state.json.forEach(listing => {
       let array = []
@@ -112,9 +92,6 @@ export default class App extends Component {
       'listings': jsonArray
     })
   }
-
-  
-
 
   openFilter = () => {
     document.getElementById('filterComponent').classList.add('filterOn')
